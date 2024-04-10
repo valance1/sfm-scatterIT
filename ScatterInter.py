9a170eaff16941b995a55772f51f2f4c2f3d6566
valance <67081858+valance1@users.noreply.github.com>
2024-04-10 12:35:58 -0300

    Update ScatterInter.py

--- ScatterInter.py
+++ ScatterInter.py
@@ -1,3 +1,5 @@
+import random, math
+
 import sfm, sfmApp, sfmConsole
 import PySide
 from PySide import QtCore, QtGui, shiboken
@@ -12,17 +14,16 @@ class scatterUI(QtGui.QWidget):
     def initUI(self):
         self.setGeometry(0, 0, 280, 352)
         self.setMinimumSize(280, 352)
-        self.setWindowTitle("Form")
-        self.setLayoutDirection(QtCore.Qt.LeftToRight)
-
         self.gridLayout = QtGui.QGridLayout(self)
         self.setupUI()
 
     def setupUI(self):
+        self.setMinimumSize(280, 352)
         #Components related to RotationBox
         self.RotationBox = QtGui.QGroupBox("Rotation")
         self.GridRotBox = QtGui.QGridLayout(self.RotationBox)
         self.RandomizeRotCheckbox = QtGui.QCheckBox("Randomize Rotation")
+        self.RandomizeRotCheckbox.stateChanged.connect(self.switchRotState)
         self.GridRotBox.addWidget(self.RandomizeRotCheckbox, 0, 0, 1, 2)
 
         self.MinRot = QtGui.QLabel("Min:")
@@ -58,17 +59,20 @@ class scatterUI(QtGui.QWidget):
         self.CloneBox = QtGui.QGroupBox("Clone selected")
         self.GridCloneBox = QtGui.QGridLayout(self.CloneBox)
         self.CopyCheckbox = QtGui.QCheckBox("Create copies of the selected models")
+        self.CopyCheckbox.stateChanged.connect(self.switchQuantityState)
         self.GridCloneBox.addWidget(self.CopyCheckbox, 0, 0, 1, 2)
 
         self.Quantity = QtGui.QLabel("Quantity:")
         self.GridCloneBox.addWidget(self.Quantity, 1, 0, 1, 1)
         self.QuantityInput = QtGui.QLineEdit()
+        self.QuantityInput.setEnabled(False)
         self.GridCloneBox.addWidget(self.QuantityInput, 1, 1, 1, 1)
 
         # Components related to PositionBox
         self.PositionBox = QtGui.QGroupBox("Position")
         self.GridPosBox = QtGui.QGridLayout(self.PositionBox)
         self.RandomizePosCheckbox = QtGui.QCheckBox("Randomize Position")
+        self.RandomizePosCheckbox.stateChanged.connect(self.switchPosState)
         self.GridPosBox.addWidget(self.RandomizePosCheckbox, 0, 0, 1, 2)
 
         self.MinPos = QtGui.QLabel("Min:")
@@ -99,6 +103,20 @@ class scatterUI(QtGui.QWidget):
         self.GridPosBox.addWidget(self.MaxZPos, 2, 5, 1, 1)
         self.MaxZPosInput = QtGui.QLineEdit()
         self.GridPosBox.addWidget(self.MaxZPosInput, 2, 6, 1, 1)
+        
+        self.MinXRotInput.setEnabled(False)
+        self.MinYRotInput.setEnabled(False)
+        self.MinZRotInput.setEnabled(False)
+        self.MaxXRotInput.setEnabled(False)
+        self.MaxYRotInput.setEnabled(False)
+        self.MaxZRotInput.setEnabled(False)
+        self.MinXPosInput.setEnabled(False)
+        self.MinYPosInput.setEnabled(False)
+        self.MinZPosInput.setEnabled(False)
+        self.MaxXPosInput.setEnabled(False)
+        self.MaxYPosInput.setEnabled(False)
+        self.MaxZPosInput.setEnabled(False)
+
 
         # Add widgets to the main grid layout
         self.gridLayout.addWidget(self.PositionBox, 3, 0, 1, 2)
@@ -106,12 +124,52 @@ class scatterUI(QtGui.QWidget):
         self.gridLayout.addWidget(self.RotationBox, 1, 0, 1, 2)
         self.scatterButton = QtGui.QPushButton("Scatter")
         self.scatterButton.setEnabled(True)
+        self.scatterButton.clicked.connect(self.scatterIT)
         self.scatterButton.setMinimumSize(QtCore.QSize(0, 30))
         self.gridLayout.addWidget(self.scatterButton, 4, 0, 1, 2)
+
+    def switchPosState(self):
+        self.MinXPosInput.setEnabled(self.RandomizePosCheckbox.isChecked())
+        self.MinYPosInput.setEnabled(self.RandomizePosCheckbox.isChecked())
+        self.MinZPosInput.setEnabled(self.RandomizePosCheckbox.isChecked())
+        self.MaxXPosInput.setEnabled(self.RandomizePosCheckbox.isChecked())
+        self.MaxYPosInput.setEnabled(self.RandomizePosCheckbox.isChecked())
+        self.MaxZPosInput.setEnabled(self.RandomizePosCheckbox.isChecked())
+    
+    def switchRotState(self):
+        self.MinXRotInput.setEnabled(self.RandomizeRotCheckbox.isChecked())
+        self.MinYRotInput.setEnabled(self.RandomizeRotCheckbox.isChecked())
+        self.MinZRotInput.setEnabled(self.RandomizeRotCheckbox.isChecked())
+        self.MaxXRotInput.setEnabled(self.RandomizeRotCheckbox.isChecked())
+        self.MaxYRotInput.setEnabled(self.RandomizeRotCheckbox.isChecked())
+        self.MaxZRotInput.setEnabled(self.RandomizeRotCheckbox.isChecked())
+
+    def switchQuantityState(self):
+        self.QuantityInput.setEnabled(self.CopyCheckbox.isChecked())
+
     def messageBox(self, message):
         mensagem = QtGui.QMessageBox()
         mensagem.setText(message)
         mensagem.exec_()
+
+    def randomize_position(min_x, max_x, min_y, max_y, min_z, max_z):
+        # Generate random coordinates for x, y, and z within the given range
+        x = random.uniform(min_x, max_x)
+        y = random.uniform(min_y, max_y)
+        z = random.uniform(min_z, max_z)
+        return x, y, z
+
+    def randomize_rotation(min_x, max_x, min_y, max_y, min_z, max_z):
+        # Generate random rotations for x, y, and z within the given range
+        x_rot = random.uniform(min_x, max_x)
+        y_rot = random.uniform(min_y, max_y)
+        z_rot = random.uniform(min_z, max_z)
+        return x_rot, y_rot, z_rot
+
+    def scatterIT(self):
+        if(self.RandomizePosCheckbox.isChecked() == False and self.RandomizeRotCheckbox.isChecked() == False):
+            self.messageBox("You did not allow the scattering of objects via position or rotation, no changes were made")
+
 
 janela = scatterUI()
 sfmApp.RegisterTabWindow( "scatterUI", "scatterIT", shiboken.getCppPointer(janela)[0] )
